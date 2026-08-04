07fdafed0454d59c6d69fe36e6357811d334dd39
zaoanmatong <2685539245@qq.com>
2026-08-04 17:04:24 +0800

    amend! 心理AI助手框架
    心理AI助手框架

---
+++ src/main/java/com/matong/PsychologicalAiAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.matong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologicalAiAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologicalAiAssistantApplication.class, args);
+    }
+
+}
